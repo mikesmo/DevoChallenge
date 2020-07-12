1cb0716c7d78cfb21f963b6b1fec6be21f4dd4ea
Michael Smolenski <mike_smo@yahoo.com>
2020-07-12 21:11:33 +0200

    isPalindrome

--- src/palindromes.js
+++ src/palindromes.js
@@ -1,4 +1,24 @@
 
 export const isPalindrome = (text) => {
-  return true;
+    let numOfChars = text.length;
+
+    // Check for odd length text - only even length text can be a palindrome.
+    if (numOfChars % 2 !== 0) {
+        return false;
+    }
+
+    let j = numOfChars / 2 - 1;
+
+    for (let posA = 0; posA <= j; posA++) {
+        let posB = numOfChars - posA - 1;
+
+        let charA = text.charAt(posA);
+        let charB = text.charAt(posB);
+
+        if (charA !== charB) {
+            return false;
+        }
+    }
+
+    return true
 };
---
+++ test/palindromes.test.js
@@ -0,0 +1,34 @@
+const assert = require('assert');
+const expect = require('chai').expect;
+
+require('@babel/register')();
+
+const {
+    isPalindrome,
+} = require('../src/palindromes');
+
+describe('Simple Math Test', () => {
+    it('should return false for odd length text', () => {
+        let text = 'abbaf';
+        let result = isPalindrome(text);
+        expect(result).to.be.false;
+    });
+
+    it('should return false for "abcd"', () => {
+        let text = 'abcd';
+        let result = isPalindrome(text);
+        expect(result).to.be.false;
+    });
+
+    it('should return false for "abca"', () => {
+        let text = 'abca';
+        let result = isPalindrome(text);
+        expect(result).to.be.false;
+    });
+
+    it('should return true for "abba"', () => {
+        let text = 'abba';
+        let result = isPalindrome(text);
+        expect(result).to.be.true;
+    });
+});
